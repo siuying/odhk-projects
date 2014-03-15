6ba5325cf23fe4ad9aa751505cfe2c31fa593e04
Francis Chong <francis@ignition.hk>
2014-03-15 10:36:33 +0800

    basic styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,12 +30,22 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    watch: {
+      scripts: {
+        files: ['coffeescript/*.coffee', 'css/*.scss'],
+        tasks: ['coffee', 'sass'],
+        options: {
+          spawn: false,
+        },
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['sass', 'coffee']);
 };
